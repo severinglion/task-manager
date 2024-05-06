34ddada8c9da85487bb8b7274456db89095b9739
Joshua Bunt <bunt.joshua@gmail.com>
2024-05-06 13:56:36 -0400

    Added task description page with a simple print out of the task data for now.

--- src/app/project/[id]/task/[taskId]/page.js
+++ src/app/project/[id]/task/[taskId]/page.js
@@ -1,22 +1,31 @@
-import { getProject } from '@/serverActions/projectActions';
+import { getProject, getUsers } from '@/serverActions/projectActions';
 import {
   Box,
   Typography,
 } from '@mui/material';
+import Link from 'next/link';
+import TaskStages from '@/model/TaskStages';
 
-
-export default function TaskEditor ({params}) {
+export default async function TaskEditor ({params}) {
   const taskId = params.taskId;
   const projectId = params.id;
 
-  const tasks = getProject(projectId);
-  const task = tasks.find(task => task.id === taskId);
-  console.log(task);
+  const proj = await getProject(projectId);
+  const users = await getUsers();
+
+  const task = proj.tasks.find(task => task.id === parseInt(taskId));
+  const stage = TaskStages.getStageString(task.stage || 0);
+  const assignedUser = users.find(u => u.id === task.assignee)
   return (
     <Box>
-      <Typography variant="h3">Task Editor</Typography>
-
+      <Link href={`/project/${projectId}`}>{proj.name}</Link>
+      <Typography variant="h3">{task.name}</Typography>
+      <Typography variant='body1'>{task.description}</Typography>
+      <Typography variant='body1'>Stage: {stage}</Typography>
+      <Typography variant='body1'>Due by: {task.dueDate}</Typography>
+      <Typography variant='body1'>Assigned To: {assignedUser.name}</Typography>
+      <Typography variant='body1'>Completed: {task.completed ? 'Yes' : 'No'}</Typography>
+      <Typography variant='body1'>Resources: None</Typography>
     </Box>
-  )
-
+  );
 }
